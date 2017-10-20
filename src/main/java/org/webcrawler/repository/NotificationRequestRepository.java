165dac1150a185a33edebe682ff45e5395c04c18
CodeProducer <testigotarantigo@gmail.com>
2017-10-20 21:13:24 +0200

    vip crawler worker

--- src/main/java/org/webcrawler/repository/NotificationRequestRepository.java
+++ src/main/java/org/webcrawler/repository/NotificationRequestRepository.java
@@ -3,6 +3,9 @@ package org.webcrawler.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.webcrawler.model.NotificationRequest;
 
+import java.util.List;
+
 public interface NotificationRequestRepository extends CrudRepository<NotificationRequest, Long> {
 
+    List<NotificationRequest> findByStatus(String status);
 }
