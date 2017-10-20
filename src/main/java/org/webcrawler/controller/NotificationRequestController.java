66c9213a6013212c61bee3130c91067f38d993fc
WallTearer <walltearer@gmail.com>
2017-10-20 14:04:44 +0200

    Initial app structure

---
+++ src/main/java/org/webcrawler/controller/NotificationRequestController.java
@@ -0,0 +1,14 @@
+package org.webcrawler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NotificationRequestController {
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "index";
+    }
+
+}
