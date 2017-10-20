2beb0b7df69c529f2418e1445b86463a69edd9b3
CodeProducer <testigotarantigo@gmail.com>
2017-10-20 20:06:32 +0200

    add url to notificationi_request table add simple logging

--- src/main/java/org/webcrawler/worker/CrawlerWorker.java
+++ src/main/java/org/webcrawler/worker/CrawlerWorker.java
@@ -1,14 +1,21 @@
 package org.webcrawler.worker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CrawlerWorker {
+    private static final Logger log = LoggerFactory.getLogger(CrawlerWorker.class);
+
 
     @Scheduled(fixedRate = 60000)
     public void crawlWebsites() {
-        System.out.println("Web sites will be crawled");
+        log.info("Web sites will be crawled");
 
-    }
+
+
+
+        }
 }
