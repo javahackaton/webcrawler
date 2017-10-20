a47067d641322cd9771ab4d6499f4b0f881e5857
WallTearer <walltearer@gmail.com>
2017-10-20 21:31:03 +0200

    Saving more fields for notification request

---
+++ src/main/java/org/webcrawler/constant/NotificationState.java
@@ -0,0 +1,9 @@
+package org.webcrawler.constant;
+
+/**
+ * Possible states of the notification.
+ * To get the string value of the constant: NotificationState.ACTIVE.name()
+ */
+public enum NotificationState {
+    ACTIVE, FOUND, NOTIFIED
+}
--- src/main/java/org/webcrawler/controller/NotificationRequestController.java
+++ src/main/java/org/webcrawler/controller/NotificationRequestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.webcrawler.constant.NotificationState;
 import org.webcrawler.model.NotificationRequest;
 import org.webcrawler.model.User;
 import org.webcrawler.repository.NotificationRequestRepository;
@@ -53,7 +54,10 @@ public class NotificationRequestController {
         }
 
         NotificationRequest nr = new NotificationRequest();
+        nr.setUrl(notificationRequest.getUrl());
         nr.setCssSelector(notificationRequest.getCssSelector());
+        nr.setContent(notificationRequest.getContent());
+        nr.setStatus(NotificationState.ACTIVE.name());
         nr.setUserId(userId);
         notificationRequestRepository.save(nr);
 
